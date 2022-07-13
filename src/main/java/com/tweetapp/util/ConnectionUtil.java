9a1d469fa2500583dc165f7cb27811271428ca00
vishnu revula <vishnurevula14@gmail.com>
2022-07-13 15:20:25 +0530

    Basic functions -dB

---
+++ src/main/java/com/tweetapp/util/ConnectionUtil.java
@@ -0,0 +1,25 @@
+package com.tweetapp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+	public static Connection getConnection() {
+		DatabaseProperties properties = new DatabaseProperties();
+		try {
+			Class.forName(properties.readProperty("driver"));
+			String url = properties.readProperty("url") + properties.readProperty("database");
+			String username = properties.readProperty("username");
+			String password = properties.readProperty("password");
+			return DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException ex) {
+			System.out.println("ClassNotFoundException - Unable to connect Database: " + ex.getMessage());
+			return null;
+		} catch (SQLException ex) {
+			System.out.println("SQLException - Unable to connect Database: " + ex.getMessage());
+			return null;
+		}
+	}
+}
